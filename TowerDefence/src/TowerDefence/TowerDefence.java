2639c11d9e2d73c1dbd496da3f7b2463c6aac931
mahulkko <mahulkko@lenny.in.fhkn.de>
2013-03-22 12:37:11 +0100

    gitignore datei geändert

--- TowerDefence/src/TowerDefence/TowerDefence.java
+++ TowerDefence/src/TowerDefence/TowerDefence.java
@@ -4,5 +4,9 @@ public class TowerDefence {
 	public static void main( String[] args )
 	  {
 		System.out.println("Towerdefence");
+		System.out.println("ist ein");
+		System.out.println("super");
+		System.out.println("Spiel");
+		System.out.println("!!!!");
 	  }
 }
