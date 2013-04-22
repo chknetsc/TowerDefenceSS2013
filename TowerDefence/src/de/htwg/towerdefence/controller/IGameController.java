6e06cbda7a8b20e758523c3dc95d7f8ed287e72f
Martin <Martin@MartinTimo>
2013-04-22 19:19:54 +0200

    controller

--- TowerDefence/src/de/htwg/towerdefence/controller/IGameController.java
+++ TowerDefence/src/de/htwg/towerdefence/controller/IGameController.java
@@ -3,8 +3,17 @@ package de.htwg.towerdefence.controller;
 import de.htwg.towerdefence.model.ITower;
 
 public interface IGameController {
+	// Towerfunktionen
 	boolean setTower(int x, int y);
+	boolean setTower(int x, int y, int damage, int range, int speed, int numberShoot, double hitrate);
+	boolean updateTowerDamage(int x, int y, int damage);
+	boolean updateTowerRange(int x, int y, int range);
+	boolean updateTowerSpeed(int x, int y, int speed);
+	boolean updateTowerNumberShoot(int x, int y, int numberShoot);
+	boolean updateTowerHitrate(int x, int y, double hitrate);
 	boolean deleteTower(int x, int y);
-	boolean updateTower(int x, int y);
-	ITower getTower(int x, int y);
+	boolean isSetTower(int x, int y);
+	// Mobfunktionen
+	boolean setMob(int x, int y);
+	boolean setMob(int x, int y, int live, int speed);
 }
--- TowerDefence/src/de/htwg/towerdefence/controller/impl/GameController.java
+++ TowerDefence/src/de/htwg/towerdefence/controller/impl/GameController.java
@@ -1,56 +1,84 @@
 package de.htwg.towerdefence.controller.impl;
 
 import de.htwg.towerdefence.controller.IGameController;
+import de.htwg.towerdefence.model.IMob;
+import de.htwg.towerdefence.model.IPlayingField;
 import de.htwg.towerdefence.model.ITower;
-import de.htwg.towerdefence.model.way.impl.CheckWay;
-import de.htwg.towerdefence.util.Coord;
+import de.htwg.towerdefence.model.impl.Mob;
+import de.htwg.towerdefence.model.impl.Tower;
 
 public class GameController implements IGameController {
-	
-	// Start Coords of the Playingfield
-	private Coord start;  
-	
-	 // End Coords of the Playingfield
-	private Coord end;  
-	
-	// CheckWay to proof the Way
-	private CheckWay way;       
+		
+	private IPlayingField field;
 	
 	public GameController() {
-		
+		this.field.initPlayingField(10, 10);
 	}
 
 	@Override
 	public boolean setTower(int x, int y) {
-		// TODO Auto-generated method stub
-		return false;
+		ITower tower = new Tower(1,1,1,1,1.0);
+		return this.field.setTower(x, y, tower);
+	}
+
+	@Override
+	public boolean setTower(int x, int y, int damage, int range, int speed,
+			int numberShoot, double hitrate) {
+		ITower tower = new Tower(damage,range,speed,numberShoot,hitrate);
+		return this.field.setTower(x, y, tower);
+	}
+
+	@Override
+	public boolean updateTowerDamage(int x, int y, int damage) {
+		return this.field.getTower(x, y).setDamage(damage);
+	}
+
+	@Override
+	public boolean updateTowerRange(int x, int y, int range) {
+		return this.field.getTower(x, y).setRange(range);
+	}
+
+	@Override
+	public boolean updateTowerSpeed(int x, int y, int speed) {
+		return this.field.getTower(x, y).setSpeed(speed);
+	}
+
+	@Override
+	public boolean updateTowerNumberShoot(int x, int y, int numberShoot) {
+		return this.field.getTower(x, y).setNumberOfShoot(numberShoot);
+	}
+
+	@Override
+	public boolean updateTowerHitrate(int x, int y, double hitrate) {
+		return this.field.getTower(x, y).setHitRate(hitrate);
 	}
 
 	@Override
 	public boolean deleteTower(int x, int y) {
-		// TODO Auto-generated method stub
+		if(this.field.deleteTower(x, y) != null) {
+			return true;
+		}
 		return false;
 	}
 
 	@Override
-	public boolean updateTower(int x, int y) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean isSetTower(int x, int y) {
+		return this.field.isSetTower(x, y);
 	}
 
 	@Override
-	public ITower getTower(int x, int y) {
-		// TODO Auto-generated method stub
-		return null;
+	public boolean setMob(int x, int y) {
+		IMob mob = new Mob();
+		mob.setLive(100);
+		mob.setSpeed(1);
+		return this.field.setMob(x, y, mob);
 	}
-	
-	/*
-	    this.start = new Coord(0,0);
-		this.end = new Coord(this.sizeX-1, this.sizeY-1);
-		this.way = new CheckWay();
-		this.way.initWayPoints(this.sizeX, this.sizeY);
-	 * 
-	 * 
-	 * 
-	 */
+
+	@Override
+	public boolean setMob(int x, int y, int live, int speed) {
+		IMob mob = new Mob();
+		mob.setLive(live);
+		mob.setSpeed(speed);
+		return this.field.setMob(x, y, mob);
+	}
 }
